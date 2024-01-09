ee80178766b5f5ef2e1b6880be578d0102430490
your name goes here <prateeky477@gmail.com>
2024-01-09 20:44:46 +0530

    update package.json

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
-  },
-  
+  build:{
+    outDir: "build"
+  }
 })
